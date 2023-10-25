5dc95da96f3ced46487eea453cbdbb45b272bdc3
1juanherrera <juansebas751@gmail.com>
2023-10-24 22:12:09 -0500

    nikeShoes frontend build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 
 export const App = () => {
+
+  const [counter, setCounter] = useState(0)
+
+  const incremented = ( number: number ): void => {
+    setCounter(counter + number)
+  }
 
   return (
     <div>
       <h1>NikeShoes - TS</h1>
+      <p>{ counter }</p>
+      <button onClick={ () => incremented(2) }>
+        + 1
+      </button>
     </div>
   )
 }
